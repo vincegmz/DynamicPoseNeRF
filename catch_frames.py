ba74506a8e696bf8b6077ed4cea82de98c028679
Daniel Wang <danwyk@Daniels-MacBook-Pro-3.local>
2023-04-02 16:40:39 -0400

    Fix catch_framse.py

--- catch_frames.py
+++ catch_frames.py
@@ -11,20 +11,20 @@ os.mkdir(image_path)
 
 capture = cv2.VideoCapture("sample.mp4")
 
-# Input: 30 fps video
+# Input: 60 fps video
 frame_no = 0
 
 while (capture.isOpened()):
     ret, frame = capture.read()
 
-    if frame_no % 2 == 0:
+    if frame_no % 10 == 0:
         target = str(os.path.join(image_path, f"{frame_no}.jpg"))
         cv2.imwrite(target, frame)
 
     frame_no += 1
 
-    # Only take first 30s of the video
-    if frame_no > 30 * 30:
+    # Only take first 60s of the video
+    if frame_no > 60 * 60:
         break
 
 capture.release()
